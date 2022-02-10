248d289aa7dde382443fbc0bfe66680d61a824db
sherinfarhana <66919963+sherinfarhana@users.noreply.github.com>
2022-02-10 17:56:10 +0300

    Do not automate sale order date

--- professional_templates/models/sale_order.py
+++ professional_templates/models/sale_order.py
@@ -34,6 +34,7 @@ from num2words import num2words
 
 from odoo import _, api, fields, models
 
+from odoo.exceptions import UserError
 
 class SO(models.Model):
     """Sales Order @Model inherited to add fields and methods for report
@@ -80,3 +81,20 @@ class SO(models.Model):
                         (rec.currency_id.currency_name or '')).upper()
             else:
                 rec.amount_words = _('Disabled')
+
+    def action_confirm(self):
+        if self._get_forbidden_state_confirm() & set(self.mapped('state')):
+            raise UserError(_(
+                'It is not allowed to confirm an order in the following states: %s'
+            ) % (', '.join(self._get_forbidden_state_confirm())))
+
+        for order in self.filtered(lambda order: order.partner_id not in order.message_partner_ids):
+            order.message_subscribe([order.partner_id.id])
+        self.write({
+            'state': 'sale',
+            # 'date_order': fields.Datetime.now()
+        })
+        self._action_confirm()
+        if self.env.user.has_group('sale.group_auto_done_setting'):
+            self.action_done()
+        return True
