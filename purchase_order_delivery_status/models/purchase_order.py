0ce7c5ac9bef83e0864c1b38a3e21f310702983f
sherinfarhana <66919963+sherinfarhana@users.noreply.github.com>
2022-02-27 13:25:00 +0300

    update purchase staus - fix rounding issues

--- purchase_order_delivery_status/models/purchase_order.py
+++ purchase_order_delivery_status/models/purchase_order.py
@@ -49,3 +49,20 @@ class PurchaseOrder(models.Model):
                 rec.delivery_status = 'received'
             elif any(p.state in ('waiting', 'confirmed') for p in pickings):
                 rec.delivery_status = 'processing'
+    
+    def update_delivery_status(self):
+        for rec in self:
+            pickings = self.env['stock.picking'].search([
+                ('purchase_id', '=', rec.id)])
+            orderlines = rec.mapped('order_line')
+            if not pickings and not orderlines.filtered(lambda x:x.product_id.type == 'service'):
+                rec.delivery_status = 'nothing'
+            elif all(o.qty_received == 0 for o in orderlines):
+                rec.delivery_status = 'to_receive'
+            elif orderlines.filtered(lambda x: x.qty_received < x.product_qty):
+                rec.delivery_status = 'partial'
+            elif all(o.qty_received >= o.product_qty for o in orderlines):
+                rec.delivery_status = 'received'
+            elif any(p.state in ('waiting', 'confirmed') for p in pickings):
+                rec.delivery_status = 'processing'
+    
