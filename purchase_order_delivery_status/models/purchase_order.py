bdff8c701066a94441371debe702443bb33bf980
Sherin Farhana <s.sherinfarhana@gmail.com>
2022-06-26 12:01:45 +0300

    force mark LPO as received

--- purchase_order_delivery_status/models/purchase_order.py
+++ purchase_order_delivery_status/models/purchase_order.py
@@ -50,7 +50,7 @@ class PurchaseOrder(models.Model):
             elif any(p.state in ('waiting', 'confirmed') for p in pickings):
                 rec.delivery_status = 'processing'
 
-    def _update_delivery_status(self):
+    def _force_update_delivery_status(self):
         for rec in self:
             rec.delivery_status = 'received'
             rec.message_post(body=_("%(name)s forcefully closed the partically delivered LPO as fully Received.", name=rec.user_id))
