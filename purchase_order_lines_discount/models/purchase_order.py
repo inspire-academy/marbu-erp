5fa118b28f0228499124e4bf3d3eac58df7cdfc8
Sherin Farhana <s.sherinfarhana@gmail.com>
2023-04-05 11:26:05 +0300

    purchase total discount

--- purchase_order_lines_discount/models/purchase_order.py
+++ purchase_order_lines_discount/models/purchase_order.py
@@ -11,7 +11,7 @@ class PurchaseOrderLine(models.Model):
     fixed_discount = fields.Float(string="Fixed Disc.", digits="Product Price", default=0.000)
 
     discount = fields.Float(string='% Disc.', digits='Discount', default=0.000)
-
+    
     @api.onchange("fixed_discount")
     def _onchange_fixed_discount(self):
         for line in self:
@@ -50,7 +50,52 @@ class PurchaseOrderLine(models.Model):
             'price_unit': price_unit_w_discount,
             'currency_id': self.order_id.currency_id,
             'product_qty': self.product_qty,
-           'product': self.product_id,
+            'product': self.product_id,
             'partner': self.order_id.partner_id,
         }
 
+class PurchaseMonetaryInherit(models.Model):
+    _inherit = 'purchase.order'
+
+    discounts = fields.Monetary(string='Total Product Discount Fixed Amount', readonly=True)
+    discount_in_percentage = fields.Monetary(string='DiscIn(%) ', readonly=True)
+    discs_price = fields.Float(string='Total Product Discount Percentage Equal Price', readonly=True)
+    amount_total = fields.Float(String='Total', compute='_get_sum')
+    discounted_price = fields.Float(string='Discounted Price')
+    amount_untaxed = fields.Monetary(string='Untaxed_Amount')
+    amount_tax = fields.Monetary(string='Taxes')
+    extra_discount_in_price = fields.Monetary(string='Order Discount Fixed Amount')
+    extra_discount_percentage = fields.Float(string='Order Discount Fixed Percentage', readonly=False)
+    ex_disc_price = fields.Monetary(string='Order Discount Fixed Amount', compute='get_extra')
+    ex_disc_perc = fields.Monetary(string='Order Discount Fixed percentage', compute='get_extra')
+    ex_dis_perc_eql_price = fields.Monetary(string='Order Discount Fixed Percentage Equal Amount',
+                                            compute='_set_extra_disc')
+
+    def _get_sum(self):
+        for disc in self:
+            disc.discounted_price = disc.amount_untaxed * (disc.discount_in_percentage / 100) * 100
+
+        for rec in self:
+            rec.amount_total = rec.amount_untaxed + rec.amount_tax - rec.discounts - rec.discs_price - rec.ex_disc_price - rec.ex_dis_perc_eql_price
+
+    @api.depends('order_line.fixed_discount', 'order_line.discount', 'order_line')
+    @api.onchange('order_line')
+    def set_discount(self):
+        self.discounts = 0
+        self.discount_in_percentage = 0
+        self.discs_price = 0
+
+        for rec in self.order_line:
+            self.discounts += rec.fixed_discount
+            self.discount_in_percentage += rec.discount / 100
+            self.discs_price += rec.price_unit
+
+    def get_extra(self):
+        for rec in self:
+            rec.ex_disc_perc = rec.extra_discount_percentage / 100
+            rec.ex_disc_price = rec.extra_discount_in_price
+
+    def _set_extra_disc(self):
+        for ext in self:
+            ext.ex_dis_perc_eql_price = (ext.amount_untaxed - ext.discs_price - ext.discounts) * (
+                    ext.extra_discount_percentage / 100)
